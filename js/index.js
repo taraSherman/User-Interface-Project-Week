bf7f482bcc319ba5f79de5af0f819c460219dcc2
Tara Sherman <taradelin@gmail.com>
2019-03-03 14:04:45 -0500

    Still having issues with menu JS and with positioning/z-index

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// JS goes here
+
+const menu = document.querySelector('.menu');
+const menuOpen = document.querySelector('.menu-open');
+const hamburger = document.querySelector('.menu-inactive');
+
+
+
+const showMenu = () => {
+    // Toggle the "menu--open" class on your menu refence. 
+    menuOpen.classList.toggle('menu--open');
+    hamburger.classList.toggle('visible');
+    event.stopPropagation;
+};
+
+// const hideMenu = () => {
+//     menu.classList.toggle()('menu--closed');
+//     hamburger.classList.toggle('visible');
+//     event.stopPropagation
+// }
+
+
+// Using your menuOpen reference, add a click handler that calls toggleMenu
+menuOpen.addEventListener('click', showMenu);
+// hamburger.addEventListener('click', hideMenu);
